b1151be1cbc89c2b61a359999de90ed4001bca9c
hailuu <hoanghai.luu.71@gmail.com>
2023-12-17 04:38:15 +0700

    init

--- script.js
+++ script.js
@@ -1,19 +1,27 @@
 if (!window.setInter) {
     window.setInter = setTimeout(() => {
 
-        var EASY_CHOICES = [
+        var EASY_CHOICES_PAGE_1 = [
             [5, 3],
             [18, 3],
             [31, 4],
             [47, 2],
-            [125, 2]
+            [128, 4],
+            [144, 4],
+            [160, 3],
+            [173, 3],
+            [197, 2],
+            [218, 2],
+            [228, 2],
         ];
 
         var MATRIX_CHOICES = [
             53,
             76,
             93,
-            104,
+            110,
+            182,
+            203,
         ];
 
         function randomInArray(arr) {
@@ -36,27 +44,33 @@ if (!window.setInter) {
             return newList;
         }
 
-        EASY_CHOICES.forEach(([start, count]) => {
-            const choices = Array.from({length: count}, (_, i) => start + i * 3);
-            document.querySelector(`#i${randomInArray(choices)}`)?.click();
+        EASY_CHOICES_PAGE_1.forEach(([start, count]) => {
+            if (start === 5) {
+                document.querySelector(`#i5`)?.click();
+            } else if (start === 18) {
+                document.querySelector(`#i24`)?.click();
+            } else if (start === 47) {
+                document.querySelector(`#i47`)?.click();
+            } else if (start === 160) {
+                document.querySelector(`#i160`)?.click();
+            } else if (start === 173) {
+                document.querySelector(`#i173`)?.click();
+            } else {
+                const choices = Array.from({length: count}, (_, i) => start + i * 3);
+                document.querySelector(`#i${randomInArray(choices)}`)?.click();
+            }
         });
 
         MATRIX_CHOICES.forEach((tableId) => {
             const rows = document.querySelectorAll(`div[aria-labelledby="i${tableId}"]  > div:nth-child(n)`);
-            // rows.forEach((row) => {
-            //     let cells = row.querySelectorAll('div[role="radio"]');
-            //     if (cells.length === 0)
-            //         cells = row.querySelectorAll('div[role="checkbox"]');
-            //     getRandomUniqueListFromNodeList(cells)?.forEach((item) => {
-            //         item?.click()
-            //     });
-            // });
             for (const row of getRandomUniqueListFromNodeList(rows)) {
                 row.querySelector("div[role='checkbox'],div[role='radio']")?.click()
             }
+
         });
 
         setTimeout(() => {
+            // Page 1
             document.querySelector('#mG61Hd > div.RH5hzf.RLS9Fe > div > div.ThHDze > div > div.lRwqcd > div')?.click();
             document.querySelector(`body > div.Uc2NEf > div:nth-child(2) > div.RH5hzf.RLS9Fe > div > div.c2gzEf > a`)?.click();
         }, 1000);
